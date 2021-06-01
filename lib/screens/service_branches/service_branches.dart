080d34c08a3c0b2ae7fd83f490c6477f6be6cb25
Ahmad-Elraqab <65804256+Ahmad-Elraqab@users.noreply.github.com>
2021-06-01 19:31:35 +0300

    bug

--- lib/screens/service_branches/service_branches.dart
+++ lib/screens/service_branches/service_branches.dart
@@ -24,49 +24,53 @@ class ServiceBranches extends StatelessWidget {
                   child: ListView.builder(
                     clipBehavior: Clip.none,
                     itemCount: 20,
-                    itemBuilder: (context, index) => Container(
-                      clipBehavior: Clip.none,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25.0),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 1,
-                            blurRadius: 7,
-                            offset: Offset(0, 5), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      padding: EdgeInsets.all(24.0),
-                      margin: EdgeInsets.all(8.0),
-                      child: Stack(
+                    itemBuilder: (context, index) => InkWell(
+                      onTap: null,
+                      child: Container(
                         clipBehavior: Clip.none,
-                        children: [
-                          TextComponent(
-                            title: 'Construction tools',
-                            fontSize: 14,
-                            textColor: Colors.black,
-                            weight: FontWeight.w600,
-                          ),
-                          Positioned(
-                            right: -40,
-                            bottom: -15,
-                            child: Container(
-                              width: 50.0,
-                              height: 50.0,
-                              margin: EdgeInsets.all(0.0),
-                              decoration: BoxDecoration(
-                                color: Color.fromRGBO(33, 39, 56, 1),
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                              child: Icon(
-                                FontAwesomeIcons.angleRight,
-                                color: Colors.white,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 5), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        padding: EdgeInsets.all(24.0),
+                        margin: EdgeInsets.all(8.0),
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            TextComponent(
+                              title: 'Construction tools',
+                              fontSize: 14,
+                              textColor: Colors.black,
+                              weight: FontWeight.w600,
+                            ),
+                            Positioned(
+                              right: -40,
+                              bottom: -15,
+                              child: Container(
+                                width: 50.0,
+                                height: 50.0,
+                                margin: EdgeInsets.all(0.0),
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(33, 39, 56, 1),
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                                child: Icon(
+                                  FontAwesomeIcons.angleRight,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
