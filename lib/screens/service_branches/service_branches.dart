6173e1885f8103ba45e4c17d7ef959e747dfad69
Ahmad-Elraqab <65804256+Ahmad-Elraqab@users.noreply.github.com>
2021-06-01 18:56:04 +0300

    service branches page in progress

--- lib/screens/service_branches/service_branches.dart
+++ lib/screens/service_branches/service_branches.dart
@@ -1,8 +1,74 @@
+import 'package:e_service_app/components/custom_return_bar.dart';
+import 'package:e_service_app/components/text_component.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ServiceBranches extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 120.0,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(24.0),
+                  color: Color.fromRGBO(237, 242, 239, 1),
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (context, index) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 5), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(24.0),
+                      margin: EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextComponent(
+                            title: 'Construction tools',
+                            fontSize: 14,
+                            textColor: Colors.black,
+                            weight: FontWeight.w600,
+                          ),
+                          Container(
+                            width: 50.0,
+                            height: 50.0,
+                            margin: EdgeInsets.all(0.0),
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(33, 39, 56, 1),
+                              borderRadius: BorderRadius.circular(50.0),
+                            ),
+                            child: Icon(
+                              FontAwesomeIcons.angleLeft,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          CustomReturnBar(),
+        ],
+      ),
+    );
   }
 }
