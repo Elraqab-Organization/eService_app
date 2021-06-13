522872ba552feaf93f29de64b264fa70cff24a58
jalalajlan <jalalajlan@gmail.com>
2021-06-14 01:25:57 +0800

    added no search result found widgets to the search_view screen

--- lib/components/search_view_screen/modal_search_view.dart
+++ lib/components/search_view_screen/modal_search_view.dart
@@ -1,5 +1,6 @@
 import 'package:e_service_app/components/search_view_screen/search_profiles_cards.dart';
 import 'package:e_service_app/model/filter_tags.dart';
+import 'package:e_service_app/model/search_profile.dart';
 import 'package:flutter/material.dart';
 
 class ShowModalBottom extends StatelessWidget {
@@ -7,10 +8,12 @@ class ShowModalBottom extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350.0,
-      child: ListView.builder(
-          itemCount: filterTags.length,
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) => SearchCardResult("Ahmed Mousa")),
+      child: usernames.isEmpty
+          ? ListView.builder(
+              itemCount: filterTags.length,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) => SearchCardResult("Ahmed Mousa"))
+          : _noResultFound(),
       decoration: BoxDecoration(
           color: Color(0xff212738),
           boxShadow: [
@@ -24,5 +27,26 @@ class ShowModalBottom extends StatelessWidget {
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0))),
     );
+  }
+
+  Widget _noResultFound() {
+    return Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.notification_important_outlined,
+          size: 60,
+          color: Colors.white,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "No Results Found",
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        )
+      ],
+    ));
   }
 }
